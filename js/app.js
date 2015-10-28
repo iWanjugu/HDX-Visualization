768a7f80e5d0281dba0443fa4bf52e1c1c316c88
Wanjugu Irene <ireneemaina@gmail.com>
2015-10-28 21:56:39 +0300

    getting and parsing theJSON file from wherever it is

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
-var data = d3.csv("unhcr-refugee-data-ken.csv")
-    .row(function(d) { return {key: d.key, value: +d.value}; })
-    .get(function(error, rows) { console.log(rows); });
-
-
-//var json = d3.csv.parseRows(data.key);
-console.log(data.row);
+$(function(){
+  function loadJSON(callback) {
+      var xobj = new XMLHttpRequest();
+          xobj.overrideMimeType("application/json");
+          xobj.open('GET', 'unhcr.json', true); // Replace 'my_data' with the path to your file
+        xobj.onreadystatechange = function () {
+            if (xobj.readyState == 4 && xobj.status == "200") {
+              // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+              callback(xobj.responseText);
+            }
+      };
+      xobj.send(null);
+   }
+   function init() {
+     var actual_JSON = [];
+     loadJSON(function(response) {
+       // Parse JSON string into object
+      actual_JSON = JSON.parse(response);
+    });
+    return actual_JSON;
+  }
+  console.log(init());
+});
